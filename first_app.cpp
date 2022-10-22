aaca120b943a15f1c52cccb0a65b0cf7eb4043fe
Angelo Nicolson <angelonicolson@outlook.com>
2022-10-22 15:55:09 +1300

    Implemented obj model loading

--- first_app.cpp
+++ first_app.cpp
@@ -57,63 +57,15 @@ namespace vex {
 
 		vkDeviceWaitIdle(vexDevice.device());
 	}
-	// temporary helper function, creates a 1x1x1 cube centered at offset
-	std::unique_ptr<VexModel> createCubeModel(VexDevice& device, glm::vec3 offset) {
-		VexModel::Data modelData{};
-		modelData.vertices = {
-			// left face (white)
-		 {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-		 {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-		 {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-		 {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-
-		 // right face (yellow)
-		 {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-		 {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-		 {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-		 {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-
-		 // top face (orange, remember y axis points down)
-		 {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-		 {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-		 {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-		 {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-
-		 // bottom face (red)
-		 {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-		 {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-		 {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-		 {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-
-		 // nose face (blue)
-		 {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-		 {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-		 {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-		 {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-
-		 // tail face (green)
-		 {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-		 {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-		 {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-		 {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-		};
-		for (auto& v : modelData.vertices) {
-			v.position += offset;
-		}
-
-		modelData.indices = { 0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
-								12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21 };
-
-		return std::make_unique<VexModel>(device, modelData);
-	}
-
+	// Loads the game models
 	void FirstApp::loadGameObjects() {
-		std::shared_ptr<VexModel> vexModel = createCubeModel(vexDevice, { .0f, .0f, .0f });
+		std::shared_ptr<VexModel> vexModel =
+			VexModel::createModelFromFile(vexDevice, "D:\\Game-Engine\\TestModels\\colored_cube.obj");
 
-		auto cube = VexGameObject::createGameObject();
-		cube.model = vexModel;
-		cube.transform.translation = { .0f, .0f, 2.5f };
-		cube.transform.scale = { .5f, .5f, .5f };
-		gameObjects.push_back(std::move(cube));
+		auto gameObj = VexGameObject::createGameObject();
+		gameObj.model = vexModel;
+		gameObj.transform.translation = { .0f, .0f, 2.5f };
+		gameObj.transform.scale = glm::vec3(0.4f);
+		gameObjects.push_back(std::move(gameObj));
 	}
-}
+}  // namespace lve
--- vex_model.cpp
+++ vex_model.cpp
@@ -1,7 +1,28 @@
 #include "vex_model.h"
 
+#include "vex_utils.h"
+
+// libs
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+// std
 #include <cassert>
 #include <cstring>
+#include <unordered_map>
+
+namespace std {
+	template <>
+	struct hash<vex::VexModel::Vertex> {
+		size_t operator()(vex::VexModel::Vertex const& vertex) const {
+			size_t seed = 0;
+			vex::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}  // namespace std
 
 namespace vex {
 	VexModel::VexModel(VexDevice& device, const VexModel::Data& builder) : vexDevice{ device } {
@@ -17,6 +38,14 @@ namespace vex {
 			vkFreeMemory(vexDevice.device(), indexBufferMemory, nullptr);
 		}
 	}
+
+	std::unique_ptr<VexModel> VexModel::createModelFromFile(
+		VexDevice& device, const std::string& filepath) {
+		Data builder{};
+		builder.loadModel(filepath);
+		std::cout << "Vertex count: " << builder.vertices.size() << "\n";
+		return std::make_unique<VexModel>(device, builder);
+	};
 
 	void VexModel::createVertexBuffers(const std::vector<Vertex>& vertices) {
 		vertexCount = static_cast<uint32_t>(vertices.size());
@@ -133,4 +162,59 @@ namespace vex {
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 		return attributeDescriptions;
 	}
-}
+	// Loading a model
+	void VexModel::Data::loadModel(const std::string& filepath) {
+		// These will be set by tiny_obj_loader and will store the results of reading a wavefront obj file
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		// Error getting thw here
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+		// This loops through each face element getting index values
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+
+				if (index.vertex_index >= 0) {
+					vertex.position = {
+					attrib.vertices[3 * index.vertex_index + 0],
+					attrib.vertices[3 * index.vertex_index + 1],
+					attrib.vertices[3 * index.vertex_index + 2],
+					};
+					// Vertex colour support
+					vertex.color = {
+					attrib.colors[3 * index.vertex_index + 0],
+					attrib.colors[3 * index.vertex_index + 1],
+					attrib.colors[3 * index.vertex_index + 2],
+					};
+				}
+				if (index.normal_index >= 0) {
+					vertex.normal = {
+					attrib.normals[3 * index.normal_index + 0],
+					attrib.normals[3 * index.normal_index + 1],
+					attrib.normals[3 * index.normal_index + 2],
+					};
+				}if (index.texcoord_index >= 0) {
+					vertex.uv = {
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
+	}
+} // namepsace vex
